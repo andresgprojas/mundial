19868be7898e3fc8fa8663323d3ce40c5fd3b13c
andresprojas <andresprojas@gmail.com>
2014-05-12 01:03:33 -0500

    Corregir bug cuando se van a cargar los pronosticos y el partido esta cerrado

--- Controller/setPronosticos.php
+++ Controller/setPronosticos.php
@@ -6,7 +6,12 @@
     if ($usuario === FALSE){
         die("0");
     }
-    $Pronosticos = new Pronosticos();
+    $Pronosticos= new Pronosticos();
+    $Partidos   = new Partidos();
+    $rtaPartido = $Partidos->getByFilter(array('CodPartido'=>$_POST['idPartido']));
+    if ($rtaPartido->getAbierto()!='1'){
+        die(utf8_encode('No se pueden cargar los pronósticos porque este partido ya fue cerrado'));
+    }
     
     $rta = $Pronosticos->getByFilter(array('NickName_Nick'=>$usuario, 'Partidos_CodPartido'=>$_POST['idPartido']));
     
