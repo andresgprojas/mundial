6ab92fe167733baf2c64cc09325e1536f7b6b6fd
AlanSGordon <AlanSGordon@users.noreply.github.com>
2014-06-12 17:01:17 -0500

    Create Resultados1_Update.sql

---
+++ Model/Resultados1_Update.sql
@@ -0,0 +1,8 @@
+update resultados set Resultado = '3'     where Partidos_CodPartido = 1 And Puntos_CodPron = 1;
+update resultados set Resultado = '1'     where Partidos_CodPartido = 1 And Puntos_CodPron = 2;
+update resultados set Resultado = '3-5'   where Partidos_CodPartido = 1 And Puntos_CodPron = 3;
+update resultados set Resultado = '0'     where Partidos_CodPartido = 1 And Puntos_CodPron = 4;
+update resultados set Resultado = '0-2'   where Partidos_CodPartido = 1 And Puntos_CodPron = 5;
+update resultados set Resultado = '24-26' where Partidos_CodPartido = 1 And Puntos_CodPron = 6;
+update resultados set Resultado = '9-11'  where Partidos_CodPartido = 1 And Puntos_CodPron = 7;
+update resultados set Resultado = '11-15' where Partidos_CodPartido = 1 And Puntos_CodPron = 8;
