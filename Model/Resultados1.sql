3284831299ca3acb64838749a2968fb7ece0eba2
AlanSGordon <AlanSGordon@users.noreply.github.com>
2014-06-12 15:52:15 -0500

    Update Resultados1.sql

--- Model/Resultados1.sql
+++ Model/Resultados1.sql
@@ -1,1 +1,8 @@
-INSERT INTO
+insert into 'resultados' values ( '1', '1' , '1');
+insert into 'resultados' values ( '1', '2' , '1');
+insert into 'resultados' values ( '1', '3' , '0-2');
+insert into 'resultados' values ( '1', '4' , '0');
+insert into 'resultados' values ( '1', '5' , '0-2');
+insert into 'resultados' values ( '1', '6' , '3-5');
+insert into 'resultados' values ( '1', '7' , '6-8');
+insert into 'resultados' values ( '1', '8' , '0-5');
