45205cf0c2a2e3a1ef9e8e8d70565337af78f7b3
AlanSGordon <AlanSGordon@users.noreply.github.com>
2014-06-26 17:15:03 -0500

    Create Octavos.sql

---
+++ Model/Octavos.sql
@@ -0,0 +1,8 @@
+INSERT INTO `partidos` VALUES ('49', '', 'BRA', 'CHI', '2014-06-28','11:00:00', '1');
+INSERT INTO `partidos` VALUES ('50', '', 'COL', 'URU', '2014-06-28','15:00:00', '1');
+INSERT INTO `partidos` VALUES ('51', '', 'NED', 'MEX', '2014-06-29','11:00:00', '1');
+INSERT INTO `partidos` VALUES ('52', '', 'CRC', 'GRE', '2014-06-29','15:00:00', '1');
+INSERT INTO `partidos` VALUES ('53', '', 'FRA', 'NGA', '2014-06-30','11:00:00', '1');
+INSERT INTO `partidos` VALUES ('54', '', 'GER', 'ALG', '2014-06-30','15:00:00', '1');
+INSERT INTO `partidos` VALUES ('55', '', 'ARG', 'SUI', '2014-07-01','11:00:00', '1');
+INSERT INTO `partidos` VALUES ('56', '', 'BEL', 'USA', '2014-06-14','15:00:00', '1');
