ae7e119face83557ddbdafade4be56522d24d687
AlanSGordon <AlanSGordon@users.noreply.github.com>
2014-07-03 12:17:50 -0500

    Create Cuartos.sql

---
+++ Model/Cuartos.sql
@@ -0,0 +1,4 @@
+INSERT INTO `partidos` VALUES ('57', '', 'BRA', 'COL', '2014-07-04','15:00:00', '1');
+INSERT INTO `partidos` VALUES ('58', '', 'FRA', 'GER', '2014-07-04','11:00:00', '1');
+INSERT INTO `partidos` VALUES ('59', '', 'NED', 'CRC', '2014-07-05','15:00:00', '1');
+INSERT INTO `partidos` VALUES ('60', '', 'ARG', 'BEL', '2014-07-05','11:00:00', '1');
